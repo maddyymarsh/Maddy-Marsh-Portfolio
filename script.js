97f08ae5c695916c52aebe670b5372bc6168de39
maddyymarsh <marshyy021@gmail.com>
2025-11-15 15:54:35 -0500

    sidebar
    sidebar & some semantic updates

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-window.onload = function(){
-    this.alert("Thank you for visiting my portfolio! Please note, I am coding this site myself and it's still a work in progress.")
+// alert for homepage
+if (window.location.href.indexOf('index.html') !== -1) {
+  alert('Hello, thank you for visiting my portfolio! I am coding this myself and it’s currently still a work in progress.');
 }
+
+// sidebar
+const sidebarBttn = document.getElementById('sidebarBttn');
+const sidebar = document.getElementById('sidebar');
+
+// Toggle function
+function toggleSidebar() {
+  sidebar.classList.toggle('close');
+  sidebarBttn.classList.toggle('rotate');
+}
+
+sidebarBttn.addEventListener('click', toggleSidebar);
