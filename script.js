0825d3f65cdbd30af8679afe4a6564c4490da79f
maddyymarsh <marshyy021@gmail.com>
2025-11-25 15:07:02 -0500

    edit sidebar / add upkeep

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ if (window.location.href.indexOf('index.html') !== -1) {
   alert('Hello, thank you for visiting my portfolio! I am coding this myself and it’s currently still a work in progress.');
 }
 
-// sidebar
-const sidebarBttn = document.getElementById('sidebarBttn');
-const sidebar = document.getElementById('sidebar');
+    const sidebar = document.getElementById('sidebar');
+    const sidebarBttn = document.getElementById('sidebarBttn');
 
-// Toggle function
-function toggleSidebar() {
-  sidebar.classList.toggle('close');
-  sidebarBttn.classList.toggle('rotate');
-}
+    // Whole sidebar clickable
+    sidebar.addEventListener('click', () => {
+        sidebar.classList.toggle('close');
+        sidebarBttn.classList.toggle('rotate');
+    });
 
-sidebarBttn.addEventListener('click', toggleSidebar);
+    // Button also clickable, but does NOT double-trigger
+    sidebarBttn.addEventListener('click', (e) => {
+        e.stopPropagation(); // prevents sidebar click from ALSO firing
+        sidebar.classList.toggle('close');
+        sidebarBttn.classList.toggle('rotate');
+    });
+
+
