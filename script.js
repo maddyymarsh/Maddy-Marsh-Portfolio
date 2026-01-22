3858344255c169336484f42195395a7436b9da97
maddyymarsh <marshyy021@gmail.com>
2026-01-22 14:26:23 -0500

    mid progress - secondary nav

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 
     const sidebar = document.getElementById('sidebar');
-    const sidebarBttn = document.getElementById('sidebarBttn');
-
-    // Whole sidebar clickable
-    sidebar.addEventListener('click', () => {
-        sidebar.classList.toggle('close');
-        sidebarBttn.classList.toggle('rotate');
-    });
 
     // Button also clickable, but does NOT double-trigger
     sidebarBttn.addEventListener('click', (e) => {
-        e.stopPropagation(); // prevents sidebar click from ALSO firing
         sidebar.classList.toggle('close');
-        sidebarBttn.classList.toggle('rotate');
     });
 
 
