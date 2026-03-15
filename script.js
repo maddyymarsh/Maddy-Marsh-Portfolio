866602df6762be26c3a186ad44093dd67f3a89b2
maddyymarsh <marshyy021@gmail.com>
2026-03-14 21:36:54 -0400

    Add PRC, update project nav, small project edits

--- script.js
+++ script.js
@@ -6,4 +6,22 @@
         sidebar.classList.toggle('close');
     });
 
+const gradient = document.querySelector(".gradient-bg");
 
+let mouseX = 50;
+let mouseY = 50;
+let ticking = false;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = (e.clientX / window.innerWidth) * 100;
+  mouseY = (e.clientY / window.innerHeight) * 100;
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      gradient.style.setProperty("--x", `${mouseX}%`);
+      gradient.style.setProperty("--y", `${mouseY}%`);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
